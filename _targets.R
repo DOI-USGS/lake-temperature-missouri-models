01f0f563f100f1d27d59f091b85c0838d7b36240
Julie Padilla <padilla410@gmail.com>
2022-06-07 15:41:40 -0400

    updates to 2_run.R

--- _targets.R
+++ _targets.R
@@ -10,10 +10,10 @@ tar_option_set(packages = c('tidyverse',
                             'ggplot2'))
 
 source('1_prep.R')
-# source('2_run.R')
+source('2_run.R')
 # source('3_extract.R')
 # source('4_visualize.R')
 # source('5_evaluate.R')
 
 # Return the complete list of targets
-c(p1)#, p2, p3, p4, p5)
+c(p1, p2)#, p3, p4, p5)
