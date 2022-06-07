a0398178d6a7c5c56ac0e4f4207319ced6ba5cfd
Julie Padilla <padilla410@gmail.com>
2022-06-07 15:06:30 -0400

    temporarily commenting out other phases and related scripts

--- _targets.R
+++ _targets.R
@@ -2,7 +2,7 @@ library(targets)
 options(clustermq.scheduler = "multicore")
 
 suppressPackageStartupMessages(library(tidyverse))
-tar_option_set(packages = c('tidyverse', 
+tar_option_set(packages = c('tidyverse',
                             'data.table',
                             'ncdfgeom',
                             'arrow',
@@ -10,10 +10,10 @@ tar_option_set(packages = c('tidyverse',
                             'ggplot2'))
 
 source('1_prep.R')
-source('2_run.R')
-source('3_extract.R')
-source('4_visualize.R')
-source('5_evaluate.R')
+# source('2_run.R')
+# source('3_extract.R')
+# source('4_visualize.R')
+# source('5_evaluate.R')
 
 # Return the complete list of targets
-c(p1, p2, p3, p4, p5)
+c(p1)#, p2, p3, p4, p5)
