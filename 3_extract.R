8460138845b9e4cbf65206d5c2350e4d11f4f811
Julie Padilla <padilla410@gmail.com>
2022-06-08 11:59:33 -0400

    add phase 3

--- 3_extract.R
+++ 3_extract.R
@@ -20,7 +20,7 @@ p3 <- list(
   tar_target(
     p3_nldas_glm_uncalibrated_output_feather_tibble,
     generate_output_tibble(p2_nldas_glm_uncalibrated_run_groups, p3_nldas_glm_uncalibrated_output_feathers,
-                           lake_xwalk = p1_lake_to_state_xwalk_df),
+                           lake_xwalk = p1_lake_to_univ_mo_xwalk_df),
     pattern = map(p2_nldas_glm_uncalibrated_run_groups, p3_nldas_glm_uncalibrated_output_feathers)
   ),
 
@@ -35,11 +35,11 @@ p3 <- list(
     format = 'file'
   ),
 
-  # Group output feather tibble by state
+  # Group output feather tibble by lake name
   tar_target(
     p3_nldas_glm_uncalibrated_output_feather_groups,
     p3_nldas_glm_uncalibrated_output_feather_tibble %>%
-      group_by(state) %>%
+      group_by(`Lake Name`) %>%
       tar_group(),
     iteration = "group"
   ),
@@ -49,7 +49,8 @@ p3 <- list(
     p3_nldas_glm_uncalibrated_output_zips,
     {
       files_to_zip <- p3_nldas_glm_uncalibrated_output_feather_groups$export_fl
-      zipfile_out <- sprintf('3_extract/out/GLM_NLDAS_%s.zip', unique(p3_nldas_glm_uncalibrated_output_feather_groups$state))
+      zipfile_out <- sprintf('3_extract/out/GLM_NLDAS_%s.zip',
+                             unique(p3_nldas_glm_uncalibrated_output_feather_groups$`Lake Name`))
       zip_output_files(files_to_zip, zipfile_out)
     },
     format = 'file',
