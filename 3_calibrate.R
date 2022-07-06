f9d3bcbca199ea2fe651177a547fbb5a6b351c35
Julie Padilla <padilla410@gmail.com>
2022-07-06 12:45:54 -0400

    add assertion to ensure met files are correctly matched

--- 3_calibrate.R
+++ 3_calibrate.R
@@ -7,10 +7,16 @@ p3 <- list(
   tar_target(
     p3_nldas_glm_calibration_runs,
     {
-      # check mapping to ensure is correct
+      # check mapping to ensure model configuration matches nml object list order
       tar_assert_identical(p1_nldas_model_config$site_id,
                            p1_nldas_nml_objects$morphometry$lake_name,
                            "p1_nldas_nml_objects site id doesn't match p1_nldas_model_config site id")
+
+      # check mapping to ensure model configuration matches nml met file
+      tar_assert_identical(basename(p1_nldas_model_config$meteo_fl),
+                           p1_nldas_nml_objects$meteorology$meteo_fl,
+                           "p1_nldas_nml_objects meteo file doesn't match p1_nldas_model_config meteo file")
+
       run_glm_cal(
         nml_obj = p1_nldas_nml_objects,
         sim_dir = '3_calibrate/out',
