8aedfc6a5d46d866d4cc881778c44bb0a2952687
Julie Padilla <padilla410@gmail.com>
2022-07-15 11:04:36 -0400

    update pipeline to run default models

--- 2_run.R
+++ 2_run.R
@@ -1,38 +1,27 @@
-source('2_run/src/run_glm3.R')
+source('3_calibrate/src/calibration_utils.R')
 
 p2 <- list(
 
-  ##### NLDAS model runs #####
-  # Function will generate file
-  # but return a tibble that includes that filename and its hash
-  # put simulation directories in sub-directory for easy deletion
   tar_target(
-    p2_nldas_glm_uncalibrated_runs,
+    p2_nldas_glm_default_runs,
     {
-      # check mapping to ensure is correct
-      tar_assert_identical(p1_nldas_model_config$site_id, p1_nldas_nml_objects$morphometry$lake_name, "p1_nldas_nml_objects site id doesn't match p1_nldas_model_config site id")
-      run_glm3_model(
-        sim_dir = '2_run/tmp/nldas_simulations',
-        nml_obj = p1_nldas_nml_objects,
-        model_config = p1_nldas_model_config,
-        export_fl_template = '2_run/tmp/GLM_%s_%s_%s.feather')
-    },
-    packages = c('retry','glmtools', 'GLM3r'),
-    pattern = map(p1_nldas_model_config, p1_nldas_nml_objects)),
+      # check mapping to ensure model configuration matches nml object list order
+      tar_assert_identical(p1_nldas_model_config$site_id,
+                           p1_nldas_nml_objects$morphometry$lake_name,
+                           "p1_nldas_nml_objects site id doesn't match p1_nldas_model_config site id")
 
-  # For bundling of results by lake in 3_extract
-  # Group model runs by lake id
-  # Discard the glm diagnostics so they don't trigger rebuilds
-  # even when the export_fl_hash is unchanged
-  # Filter to successful model runs
-  tar_target(
-    p2_nldas_glm_uncalibrated_run_groups,
-    p2_nldas_glm_uncalibrated_runs %>%
-      select(site_id, driver, time_period, driver_start_date, driver_end_date,
-             export_fl, export_fl_hash, glm_success) %>%
-      filter(glm_success) %>%
-      group_by(site_id) %>% # Group by site_id for creating export files
-      tar_group(),
-    iteration = "group"
-  )
+      # check mapping to ensure model configuration matches nml met file
+      tar_assert_identical(basename(p1_nldas_model_config$meteo_fl),
+                           p1_nldas_nml_objects$meteorology$meteo_fl,
+                           "p1_nldas_nml_objects meteo file doesn't match p1_nldas_model_config meteo file")
+
+      run_glm_cal(
+        nml_obj = p1_nldas_nml_objects,
+        sim_dir = '2_run/out',
+        cal_parscale = c('cd' = 0.0001, 'sw_factor' = 0.02, 'Kw' = 0.01),
+        model_config = p1_nldas_model_config,
+        optimize = FALSE)
+    },
+    pattern = map(p1_nldas_model_config, p1_nldas_nml_objects))
+
 )
--- _targets.R
+++ _targets.R
@@ -13,12 +13,11 @@ tar_option_set(packages = c('tidyverse',
                             'glmtools'))
 
 source('1_prep.R')
-# source('2_run.R')
-source('3_calibrate.R')
+source('2_run.R')
+# source('3_calibrate.R')
 # source('3_extract.R')
 # source('4_visualize.R')
 # source('5_evaluate.R')
 
 # Return the complete list of targets
-
-c(p1, p3)#, p4, p5)
+c(p1, p2)#p3, p4, p5)
