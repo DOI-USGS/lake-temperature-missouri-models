19297270a1aeb4f45dd54e113715b586023e7688
Julie Padilla <padilla410@gmail.com>
2022-08-07 14:07:04 -0400

    modify the munge_obs fxns to allow file paths and objects

--- 1_prep/src/munge_obs.R
+++ 1_prep/src/munge_obs.R
@@ -1,19 +1,24 @@
 #' Create a lake-specific dataset from the complete dataset
 #'
-#' @param data chr, full file path for the complete dataset
+#' @param data chr or object, if chr use full file path for the complete dataset
 #' @param site_id chr, site id for the lake of interest
 #' @param path_out chr, subfolder for writing the lake-specific dataset
 #'
 subset_model_obs_data <- function(data, site_id, path_out = '1_prep/out/field_data_all') {
 
+  # renaming this variable to avoid confusion later
   site_id_filter <- site_id
 
-  out_fl_name <- sprintf('1_prep/out/field_data_%s.rds', site_id_filter)
+  out_fl_name <- sprintf('%s/field_data_%s.rds', path_out, site_id_filter)
 
   # check for path_out directory and create if not found
   if(!dir.exists(path_out)){dir.create(path_out)}
 
-  arrow::read_feather(data) %>%
+  if('character' %in% class(data)) {
+    data <- arrow::read_feather(data)
+  }
+
+  data %>%
     dplyr::filter(site_id %in% site_id_filter) %>%
     as.data.frame(.) %>%
     select(DateTime = date, Depth = depth, temp = temp) %>%
