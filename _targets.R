b031996a90fa8629c701204889f3eef07b99d9fe
Julie Padilla <padilla410@gmail.com>
2022-08-19 13:56:40 -0400

    forgotten targets file

--- _targets.R
+++ _targets.R
@@ -17,7 +17,7 @@ tar_option_set(packages = c('tidyverse',
 
 source('1_prep.R')
 source('2_run.R')
-# source('3_calibrate.R')
+source('3_calibrate.R')
 # source('3_extract.R')
 # source('4_visualize.R')
 # source('5_evaluate.R')
@@ -25,5 +25,5 @@ source('2_run.R')
 # Return the complete list of targets
 
 
-c(p1, p2)#, p3, p4, p5)
+c(p1, p2, p3) #, p4, p5)
 
